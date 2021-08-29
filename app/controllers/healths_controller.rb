3523c052f3e2c1b94c134179e8eaed3e71b07101
yabuuuu <475sunflower@gmail.com>
2021-08-29 23:33:15 +0900

    ランキング表示１

--- app/controllers/healths_controller.rb
+++ app/controllers/healths_controller.rb
@@ -1,6 +1,7 @@
 class HealthsController < ApplicationController
   def index
     @data = {'炭水化物:50〜65%' => 57,  '脂質:20〜30%' => 25, 'タンパク質:13〜20%' => 16}
+    @articles = Article.includes(:user).order("created_at DESC")
   end
   
 end
